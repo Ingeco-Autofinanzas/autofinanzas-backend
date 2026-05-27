3444b2bf49a31c80ba8304e7643270d7274ee4a8
Dayro Richard Rios Piñan <dayro.rios.p@gmail.com>
2026-05-27 02:51:26 -0500

    chore: initial commit

---
+++ src/main/java/com/autofinanzas/platform/AutofinanzasPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.autofinanzas.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofinanzasPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofinanzasPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autofinanzas/platform/AutofinanzasPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autofinanzas.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofinanzasPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
